ef0a92692c767201c4bde84a7a50c5df87b6365e
muhammedajsalk <ajsalmuhammed747@gmail.com>
2025-06-05 12:25:25 +0530

    basic spawn Run JavaScript files from Node

---
+++ aju.js
@@ -0,0 +1,2 @@
+console.log("first arguemet:",process.argv[2])
+console.log("second arguemet:",process.argv[3])
--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log(process)
+const {spawn}=require('child_process')
+
+const high=spawn('node',['aju.js',"ajsal","jaseel"])
+
+high.stdout.on('data',(data)=>{
+    console.log(`output:${data}`)
+})
+
+high.stderr.on('error',(err)=>{
+    console.log(`error:${err}`)
+})
